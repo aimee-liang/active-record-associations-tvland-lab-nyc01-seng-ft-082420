6e2a0fd43dc92d1e2b4b15b0b0496bd6b6e1d5f5
aimee-liang <aimeeeliang@gmail.com>
2020-09-04 10:55:23 -0400

    First commit

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+    # returns an Array of the full names of each actor associated with a show
+    end
+
 end
